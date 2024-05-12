0a8adc9bff1049f180adccd9d470208f82f616ff
Chekryzhova Ekaterina <kat9ra38@gmail.com>
2024-05-12 17:16:51 +0300

    Sprint_7_commit_2

--- scooter_api.py
+++ scooter_api.py
@@ -1,20 +1,24 @@
 import requests
 import urls
-import pytest
+import allure
 
 class ScooterApi:
+    @allure.step('Создаём курьера')
     @staticmethod
     def create_courier(body):
         return requests.post(urls.BASE_URL + urls.CREATE_COURIER_ENDPOINT, json=body)
 
+    @allure.step('Авторизуемся курьером')
     @staticmethod
     def login_courier(auth_body):
         return requests.post(urls.BASE_URL + urls.LOGIN_COURIER_ENDPOINT, json=auth_body)
 
+    @allure.step('Создаём заказ')
     @staticmethod
     def create_order(body):
         return requests.post(urls.BASE_URL + urls.CREATE_ORDER_ENDPOINT, json=body)
 
+    @allure.step('Получаем список заказов')
     @staticmethod
     def get_order_list():
         return requests.get(urls.BASE_URL + urls.GET_ORDER_LIST_ENDPOINT)
